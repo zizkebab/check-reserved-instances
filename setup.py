18e8fb3f97dcb10851ee3f112e60cdcb73c13e48
Adam Stauffer <adam@terbiumlabs.com>
2016-09-23 16:00:28 -0400

    change setup.py to reflect new README

--- setup.py
+++ setup.py
@@ -1,51 +1,63 @@
 """Setup file for PyPi."""
-from setuptools import find_packages, setup
+import io
+import setuptools
 
-setup(
-    name='check-reserved-instances',
-    license='BSD',
-    description=('Compare instance reservations and running instances for AWS '
-                 'services'),
-    author='Terbium Labs',
-    author_email='developers@terbiumlabs.com',
-    url='https://github.com/TerbiumLabs/check-reserved-instances',
-    keywords='ec2 elasticache rds aws reserved instances amazon web services',
-    platforms=['Any'],
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'boto',
-        'boto3',
-        'click',
-        'configparser',
-        'Jinja2',
-        'MarkupSafe',
-        'python-dateutil'
-    ],
-    tests_require=[
-        'mock',
-        'pytest',
-        'pytest-cov'
-    ],
-    entry_points={
-        'console_scripts': ['check-reserved-instances = '
-                            'check_reserved_instances:cli']
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ]
-)
+
+def setup():
+    """Setup function for PyPi."""
+    with io.open('README.rst') as fp:
+        readme = fp.read()
+    setuptools.setup(
+        name='check-reserved-instances',
+        license='BSD',
+        description=(
+            'Compare instance reservations and running instances for AWS '
+            'services'),
+        long_description=readme + '\n',
+        author='Terbium Labs',
+        author_email='developers@terbiumlabs.com',
+        url='https://github.com/TerbiumLabs/check-reserved-instances',
+        keywords=(
+            'ec2 elasticache rds aws reserved instances amazon web services'),
+        platforms=['Any'],
+        packages=setuptools.find_packages('src'),
+        package_dir={'': 'src'},
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            'boto',
+            'boto3',
+            'click',
+            'configparser',
+            'Jinja2',
+            'MarkupSafe',
+            'python-dateutil'
+        ],
+        tests_require=[
+            'mock',
+            'pytest',
+            'pytest-cov'
+        ],
+        entry_points={
+            'console_scripts': ['check-reserved-instances = '
+                                'check_reserved_instances:cli']
+        },
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ]
+    )
+
+if __name__ == '__main__':
+    setup()
